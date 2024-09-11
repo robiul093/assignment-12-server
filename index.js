2e2590c9d840b87e4f110ca1b28f420ef218b344
Robiul@Hasan <robihasan093@gmail.com>
2024-09-11 21:21:01 +0600

    first commit in this new repo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 
@@ -49,42 +50,53 @@ async function run() {
 
 
     // admin api
-    app.get('/allUsers', async (req, res) =>{
+    app.get('/allUsers', async (req, res) => {
       const result = await userCollection.find().toArray()
       res.send(result);
     })
 
 
-    app.put('/userRole/:id', async (req, res) =>{
-      const {id} = req.params
+    app.put('/userRole/:id', async (req, res) => {
+      const { id } = req.params
       const updateRole = req.body;
       console.log(id, updateRole.role);
-      const filter = {_id : new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
 
       const updateDoc = {
         $set: {
-          role : updateRole.role,
+          role: updateRole.role,
         },
       };
 
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
-    
-    
+
+
     //  user related api
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
       const existingUser = await userCollection.findOne(query)
 
-      
+
       if (existingUser) {
         console.log(existingUser);
         return res.send({ message: 'user already exists', insertedId: null })
       }
       const result = await userCollection.insertOne(user);
 
+      res.send(result);
+
+    })
+
+    app.delete('/deletUser/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+
+      const filter = { _id: new ObjectId(id) }
+
+      const result = await userCollection.deleteOne(filter)
       res.send(result);
 
     })
@@ -127,32 +139,51 @@ async function run() {
     })
 
 
-    app.put(`/surveyAnswer/:id`, async (req, res) =>{
+    app.put(`/surveyAnswer/:id`, async (req, res) => {
       const id = req.params.id;
       const answer = req.body;
-      
-      const question1Ans = answer.question1Ans;
-      const question2Ans = answer.question2Ans;
-      const question3Ans = answer.question3Ans;
-      console.log(answer, id, question1Ans, question2Ans, question3Ans);
+
+      const totalVote = answer.total;
+      const question1Ans = answer.question1;
+      const question2Ans = answer.question2;
+      const question3Ans = answer.question3;
+      console.log(answer, totalVote);
 
       const filter = { _id: new ObjectId(id) };
-      
+
       const updateDoc = {
 
-        $inc : {
-          'question.question_1.question1Ans.yes' : question1Ans === 'yes' ? 1 : 0,
-          'question.question_1.question1Ans.no' : question1Ans === 'no' ? 1 : 0,
-          'question.question_2.question2Ans.yes' : question2Ans === 'yes' ? 1 : 0,
-          'question.question_2.question2Ans.no' : question2Ans === 'no' ? 1 : 0,
-          'question.question_3.question3Ans.yes' : question3Ans === 'yes' ? 1 : 0,
-          'question.question_3.question3Ans.no' : question3Ans === 'no' ? 1 : 0,
+        $inc: {
+          'totalVote': totalVote,
+          'question.question_1.question1Ans.yes': question1Ans === 'yes' ? 1 : 0,
+          'question.question_1.question1Ans.no': question1Ans === 'no' ? 1 : 0,
+          'question.question_2.question2Ans.yes': question2Ans === 'yes' ? 1 : 0,
+          'question.question_2.question2Ans.no': question2Ans === 'no' ? 1 : 0,
+          'question.question_3.question3Ans.yes': question3Ans === 'yes' ? 1 : 0,
+          'question.question_3.question3Ans.no': question3Ans === 'no' ? 1 : 0,
         }
       }
-      
+
 
       const result = await surveyCollection.updateOne(filter, updateDoc)
       res.send(result)
+    })
+
+
+    // payment intent
+    app.post('/create-payment-intent', async(req, res) =>{
+      const {price} = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntent.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+      })
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      })
     })
 
 
